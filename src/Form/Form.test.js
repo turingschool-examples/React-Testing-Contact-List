cde08dc02bd626d64556a2460630c5b60c105935
Khalid Williams <khalidwill27@gmail.com>
2020-09-08 18:17:24 -0600

    Remove unit tests on form

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -1,20 +1,9 @@
 //What should we import?
 
 describe('Form', () => {
-  it('Should render a form', () => {
+  it('Should do something', () => {
 
   });
 
-  it('Should have inputs that hold their value when interacted with', () => {
-
-  });
-
-  it('Should be able to submit the form', () => {
-
-  });
-
-  it('Should clear the inputs on submit', () => {
-
-  });
 
 });
