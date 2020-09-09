2c9bb834f8953e2fb112d6be2bd0ddbb567fc5c9
Khalid Williams <khalidwill27@gmail.com>
2020-09-08 18:15:10 -0600

    Remove unit test blocks

--- src/ContactCard/ContactCard.test.js
+++ src/ContactCard/ContactCard.test.js
@@ -2,23 +2,10 @@
 
 
 describe('ContactCard', () => {
-  it('Should render a card', () => {
-
-  });
-
-  it('Should be able to click the favorite button', () => {
-
-  });
-
-  it('Should be able to click the delete button', () => {
-
-  });
-
-  it('Should render the correct UI if the contact is NOT a favorite', () => {
-    
-  });
-
-  it('Should render the correct UI if the contact is a favorite', () => {
-    
-  });
+  
+ it('should do something?', () => {
+  
+ });
+  
+  
 });
